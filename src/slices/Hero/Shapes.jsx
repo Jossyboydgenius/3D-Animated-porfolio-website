466256fb458b2531503d5640777b40746df07361
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-18 05:37:07 +0000

    Commit

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -9,7 +9,8 @@ import { Suspense } from "react"
 export default function Shapes() {
     return (
         <div className="row-span-1 row-start-1 -mt-9 aspect-square md:col-span-1 md:col-start-2 md:mt-0">
-            <Canvas className="z-0" shadows gl={{ antialias: false }} dpr={1, 1.5} camera={{ position: [0, 0, 25], 
+            <Canvas 
+            className="z-0" shadows gl={{ antialias: false }} dpr={1, 1.5} camera={{ position: [0, 0, 25], 
             fov: 30, near: 1, far: 40 }}>
             <Suspense fallback={null}>
                 <ContactShadows
