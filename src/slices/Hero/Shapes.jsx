cf41201684e1bc5141dbe30089bab26355f2b81e
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-18 05:18:19 +0000

    Commit

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -5,4 +5,8 @@ import { Canvas } from "@react-three/fiber"
 import { ContactShadows, Float, Environment } from "@react-three/drei"
 import { gsap } from "gsap/gsap-core"
 
-e
+export function Shapes({
+    return (
+        
+    )
+})
