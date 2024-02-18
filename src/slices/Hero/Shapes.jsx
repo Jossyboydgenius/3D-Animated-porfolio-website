95b37926edf1c7f19da4bef46e39b180c3a670ba
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-18 05:14:45 +0000

    Commit

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -3,3 +3,4 @@
 import * as THREE from "three"
 import { Canvas } from "@react-three/fiber"
 import { ContactShadows, Float, Environment } from "@react-three/drei"
+import { gsap } from "gsap/gsap-core"
