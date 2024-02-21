333a39645e359e19705534ce08406cac8644c342
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-21 11:13:41 +0000

    Commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,21 @@
 import React from "react";
 import { createClient } from "@/prismicio";
 import Link from "next/link";
+
+export default async function Header () {
+    const client = createClient();
+    const settings = await client.getSingle("settings");
+    return (
+        <header className="top-0 z-50 mx-auto max-w-7x1 md:sticky md: top-4">
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/" aria-label="Home page">
+                            {settings.data.name}
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
