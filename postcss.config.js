e50a2e4ed81cff6e3b045c3aa2ec38ffd88ee653
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-25 16:48:24 +0000

    Commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,6 @@
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// };
-
-
-// postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {
-      content: [
-        './app/**/*.{js,ts,jsx,tsx}',
-        './pages/**/*.{js,ts,jsx,tsx}',
-      ],
-    },
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
