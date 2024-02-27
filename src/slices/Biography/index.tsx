abb34a18a96990752fe63b04cf6e103165f3151b
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-27 17:52:12 +0000

    Commit

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -21,7 +21,7 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
       data-slice-variation={slice.variation}
     >
       <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr, 1fr]">
-        <Heading as="h1" size="xl" className="col-start-1>">
+        <Heading as="h1" size="xl" className="col-start-1">
           {slice.primary.heading}
         </Heading>
         <div className="prose prose-xl prose-slate prose-invert col-start-1">
