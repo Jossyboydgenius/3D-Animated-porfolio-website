41df49e87dc766360a3484a44cecafa17ec05cad
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-27 17:49:52 +0000

    Commit

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,29 +1,30 @@
 import clsx from "clsx";
 
 type HeadingProps = {
- as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
- size?: "xl" | "lg" | "md" | "sm";
- children: React.ReactNode;
- className?: string;
+  as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  size?: "xl" | "lg" | "md" | "sm";
+  children: React.ReactNode;
+  className?: string;
 };
+
 export default function Heading({
-    as: Comp = "h1",
-    className,
-    children,
-    size = "lg",
+  as: Comp = "h1",
+  className,
+  children,
+  size = "lg",
 }: HeadingProps) {
-    return (
-      <Comp
-        className = {clsx(
-            "font-bold leading-tight tracking-tight text-slate-300",
-            size === "xl" && "text-7x1 md:text-9x1",
-            size === "lg" && "text-6x1 md:text-8x1",
-            size === "md" && "text-5x1 md:text-6x1",
-            size === "sm" && "text-3x1 md:text-4x1",
-            className,
-        )}
-        >
-            {children}
-        </Comp>
-        );
-     }
+  return (
+    <Comp
+      className={clsx(
+        "font-bold leading-tight tracking-tight  text-slate-300",
+        size === "xl" && "text-7xl md:text-9xl",
+        size === "lg" && "text-6xl md:text-8xl",
+        size === "md" && "text-5xl md:text-6xl",
+        size === "sm" && "text-3xl md:text-4xl",
+        className,
+      )}
+    >
+      {children}
+    </Comp>
+  );
+}
