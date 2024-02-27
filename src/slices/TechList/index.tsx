4b1481f791bebe7141cfa23a13179fe1f8c97e47
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-27 17:54:58 +0000

    Commit

--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -1,87 +1,87 @@
-"use client"
+// "use client"
 
 
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
-import React, { useEffect, useRef } from "react";
-import { MdCircle } from "react-icons/md";
-import { gsap } from "gsap/gsap-core";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { Content } from "@prismicio/client";
+// import { SliceComponentProps } from "@prismicio/react";
+// import React, { useEffect, useRef } from "react";
+// import { MdCircle } from "react-icons/md";
+// import { gsap } from "gsap/gsap-core";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import Bounded from "@/components/Bounded";
-import Heading from "@/components/Heading";
+// import Bounded from "@/components/Bounded";
+// import Heading from "@/components/Heading";
 
-gsap.registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(ScrollTrigger)
 
-/**
- * Props for `TechList`.
- */
-export type TechListProps = SliceComponentProps<Content.TechListSlice>;
+// /**
+//  * Props for `TechList`.
+//  */
+// export type TechListProps = SliceComponentProps<Content.TechListSlice>;
 
-/**
- * Component for "TechList" Slices.
- */
-const TechList = ({ slice }: TechListProps): JSX.Element => {
-  const component = useRef(null)
+// /**
+//  * Component for "TechList" Slices.
+//  */
+// const TechList = ({ slice }: TechListProps): JSX.Element => {
+//   const component = useRef(null)
 
-  useEffect(()=>{
-    let ctx = gsap.context(()=>{
+//   useEffect(()=>{
+//     let ctx = gsap.context(()=>{
 
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          markers: true,
-          start: "top bottom",
-          end: "bottom top,",
-          scrub: 4,
-        },
-      });
+//       const tl = gsap.timeline({
+//         scrollTrigger: {
+//           markers: true,
+//           start: "top bottom",
+//           end: "bottom top,",
+//           scrub: 4,
+//         },
+//       });
 
-      tl.fromTo(
-        ".tech-row",
-        
-      )
+//       tl.fromTo(
+//         ".tech-row",
 
-    }, component)
-    return ()=> ctx.revert() //cleanup!
-  })
+//       )
 
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-      className="overflow-hidden"
-      ref={component}
-    >
-      <Bounded as="div">
-      <Heading size="lg" className="mb-8" as="h2">
-        {slice.primary.heading}
-      </Heading>
-       </Bounded>
-      {slice.items.map(({ tech_color, tech_name }, index) => (
-        <div
-          key={index}
-          className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
-          aria-label={tech_name || undefined}
-        >
-          {Array.from({ length: 15 }, (_, index) => (
-            <React.Fragment key={index}>
-              <span
-                className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
-                style={{
-                  color: index === 7 && tech_color ? tech_color : "inherit",
-                }}
-              >
-                {tech_name}
-              </span>
-              <span className="text-3xl">
-                <MdCircle />
-              </span>
-            </React.Fragment>
-          ))}
-        </div>
-      ))}
-    </section>
-  );
-};
+//     }, component)
+//     return ()=> ctx.revert() //cleanup!
+//   })
 
-export default TechList;
+//   return (
+//     <section
+//       data-slice-type={slice.slice_type}
+//       data-slice-variation={slice.variation}
+//       className="overflow-hidden"
+//       ref={component}
+//     >
+//       <Bounded as="div">
+//       <Heading size="lg" className="mb-8" as="h2">
+//         {slice.primary.heading}
+//       </Heading>
+//        </Bounded>
+//       {slice.items.map(({ tech_color, tech_name }, index) => (
+//         <div
+//           key={index}
+//           className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+//           aria-label={tech_name || undefined}
+//         >
+//           {Array.from({ length: 15 }, (_, index) => (
+//             <React.Fragment key={index}>
+//               <span
+//                 className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+//                 style={{
+//                   color: index === 7 && tech_color ? tech_color : "inherit",
+//                 }}
+//               >
+//                 {tech_name}
+//               </span>
+//               <span className="text-3xl">
+//                 <MdCircle />
+//               </span>
+//             </React.Fragment>
+//           ))}
+//         </div>
+//       ))}
+//     </section>
+//   );
+// };
+
+// export default TechList;
