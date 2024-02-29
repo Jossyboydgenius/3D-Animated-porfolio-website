65ed28a1029d03dfce62325f15e6972cda5369f2
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-29 15:53:24 +0000

    Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,17 +16,3 @@ const config: Config = {
 };
 export default config;
 
-
-// import type { Config } from "tailwindcss";
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {},
-//   plugins: [require("@tailwindcss/typography")],
-// };
-// export default config;
