cc5bdea5df99ead1bdaf7f57317dc16e88e7e64e
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-02-29 15:59:21 +0000

    Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import typography from '@tailwindcss/typography';
 
 const config: Config = {
   content: [
@@ -9,10 +8,38 @@ const config: Config = {
     "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {},
-  plugins: [
-    require("@tailwindcss/typography")
-  ],
-  // plugins: [typography],
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
+
+
+
+
+
+
+// import type { Config } from "tailwindcss";
+// import typography from '@tailwindcss/typography';
+
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radical": "radical-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     }
+//   },
+//   plugins: [
+//     require("@tailwindcss/typography")
+//   ],
+//   // plugins: [typography],
+// };
+// export default config;
 
