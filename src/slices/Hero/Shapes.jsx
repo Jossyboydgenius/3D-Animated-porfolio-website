573583955583640cb4b017fdca8c013ff5a6ad37
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-01 15:39:31 +0000

    Commit

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -3,8 +3,8 @@
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { ContactShadows, Float, Environment } from "@react-three/drei";
-import { gsap } from "gsap";
 import { Suspense, useRef, useState, useEffect } from "react"; // <-- Added useEffect import
+import { gsap } from "gsap";
 import { func } from "three/examples/jsm/nodes/Nodes.js";
 
 export default function Shapes() {
