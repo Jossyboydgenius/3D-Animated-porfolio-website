2133c4ae2f75eec42e8a7871fdc6abf1a2bea1e4
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-01 18:39:12 +0000

    Commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,34 +12,3 @@ const config: Config = {
 };
 export default config;
 
-
-
-
-
-
-// import type { Config } from "tailwindcss";
-// import typography from '@tailwindcss/typography';
-
-// const config: Config = {
-//   content: [
-//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./src/slices/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       backgroundImage: {
-//         "gradient-radical": "radical-gradient(var(--tw-gradient-stops))",
-//         "gradient-conic":
-//         "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-//       },
-//     }
-//   },
-//   plugins: [
-//     require("@tailwindcss/typography")
-//   ],
-//   // plugins: [typography],
-// };
-// export default config;
-
