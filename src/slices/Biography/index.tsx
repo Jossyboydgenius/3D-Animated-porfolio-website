b1bceb7a2e0024634f4a4c29499c86cc4514933c
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-03 17:15:14 +0000

    Commit

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -16,7 +16,9 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="grid"></div>
+      <div className="grid gap-x-8 gap-y-6 md:grid-cols-[2fr,1fr]">
+        
+      </div>
     </Bounded>
   );
 };
