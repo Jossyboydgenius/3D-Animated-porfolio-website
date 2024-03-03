f98f60eccfa17cdb0a056dfd84e5137ed688e6ce
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-03 23:32:29 +0000

    Commit

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,14 +1,37 @@
+import { Content } from "@prismicio/client";
 import React from "react";
 
-export default function ContentList() {
-    return 
-        <div>
-            <ul>
-                <li>
-                    <a href="">
-                        <div></div>
-                    </a>
-                </li>
-            </ul>
-        </div>
+type ContentListProps = {
+  items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+};
+
+export default function ContentList({
+  items,
+  contentType,
+  fallback_item_image,
+  view_more_text = "Read More",
+}: ContentListProps) {
+  return;
+  <div>
+    <ul>
+        {items.map((items, index)=>(
+      <li key={index}>
+        <a href="">
+          <div>
+            <span>{items.data.title}</span>
+            <div>
+                {items.tags.map((tag, index)=>(
+                    <span key={index}>{tag}</span>
+                ))}
+            </div>
+          </div>
+          <span>{viewMoreText}</span>
+        </a>
+      </li>
+        ))}
+    </ul>
+  </div>;
 }
