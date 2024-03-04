08f540bc03a6cf061882605a5286cf1dbfdd7985
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-04 14:52:16 +0000

    Commit

--- src/slices/Biography/index.tsx
+++ src/slices/Biography/index.tsx
@@ -2,6 +2,7 @@ import Bounded from "@/components/Bounded";
 import Button from "@/components/Button";
 import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
@@ -23,13 +24,17 @@ const Biography = ({ slice }: BiographyProps): JSX.Element => {
           {slice.primary.heading}
         </Heading>
         <div className="prose prose-xl prose-slate prose-invert col-start-1">
-         <PrismicRichText field={slice.primary.description} />
+          <PrismicRichText field={slice.primary.description} />
         </div>
         <Button
-        linkField={slice.primary.button_link}
-        label={slice.primary.button_text}
+          linkField={slice.primary.button_link}
+          label={slice.primary.button_text}
         />
-        
+
+        <PrismicNextImage
+          field={slice.primary.avatar}
+          className="row-start-1 max-w-sm md:col-start-2 md:row-end-3"
+        />
       </div>
     </Bounded>
   );
