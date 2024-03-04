d67a0eea91d4f484355851ffe3116ee2819104f4
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-04 18:12:16 +0000

    Commit

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -3,7 +3,8 @@
 import { ImageField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
-import { Component, useRef } from "react";
+import gsap from "gsap";
+import { Component, useEffect, useRef } from "react";
 
 type AvatarProps = {
   image: ImageField;
@@ -12,6 +13,17 @@ type AvatarProps = {
 
 export default function Avatar({ image, className }: AvatarProps) {
   const component = useRef(null);
+
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.fromTo(
+        ".avatar",
+        { opacity: 0, scale: 1.4 },
+        { scale: 1, opacity: 1, duration: 1.3, ease: "power3.inOut" }
+      );
+    });
+  });
+
   return (
     <div ref={component} className={clsx("relative h-full w-full", className)}>
       <div className="avatar aspect-square overflow-hidden rounded-3xl border-2 border-slate-700 opacity-0f">
