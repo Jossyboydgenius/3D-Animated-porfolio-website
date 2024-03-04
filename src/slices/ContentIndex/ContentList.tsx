00bdc5b09bb91bfdd2df9c087a42af45606d1129
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-04 00:01:56 +0000

    Commit

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,4 +1,5 @@
 import { Content } from "@prismicio/client";
+import Link from "next/link";
 import React from "react";
 import { MdArrowOutward } from "react-icons/md";
 
@@ -15,15 +16,20 @@ export default function ContentList({
   fallbackItemImage,
   viewMoreText = "Read More",
 }: ContentListProps) {
+    const urlPrefix = contentType === "Blog" ? "/blog" : "/project"
+
   return;
   <div>
     <ul className="grid border-b border-b-slate-100">
       {items.map((items, index) => (
+        <>
         <li key={index} className="list-item opacity-0f">
-          <a
-            href=""
+          <Link
+            href={urlPrefix + "/" + item.uid}
             className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-          >
+            aria-label="item.data.title ||"
+         
+         >
             <div className="flex flex-col">
               <span className="text-3xl font-bold">{items.data.title}</span>
               <div className="flex gap-3 text-yellow-400 text-lg font-bold">
@@ -35,8 +41,10 @@ export default function ContentList({
             <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
               {viewMoreText} <MdArrowOutward />
             </span>
-          </a>
+          </Link>
         </li>
+        </>
+        
       ))}
     </ul>
   </div>;
