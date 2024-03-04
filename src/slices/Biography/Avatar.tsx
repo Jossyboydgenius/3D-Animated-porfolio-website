1630918503cd1ca6baf256c2d7db4bb8359560ce
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-04 18:31:43 +0000

    Commit

--- src/slices/Biography/Avatar.tsx
+++ src/slices/Biography/Avatar.tsx
@@ -21,8 +21,45 @@ export default function Avatar({ image, className }: AvatarProps) {
         { opacity: 0, scale: 1.4 },
         { scale: 1, opacity: 1, duration: 1.3, ease: "power3.inOut" }
       );
-    });
-  });
+
+      window.onmousemove = (e) => {
+        if (!component.current) return;
+        const componentRect = (
+          component.current as HTMLElement
+        ).getBoundingClientRect();
+        const componentCenterX = componentRect.left + componentRect.width / 2;
+
+        let componentPercent = {
+          x: (e.clientX - componentCenterX) / componentRect.width / 2,
+        };
+
+        let distFromCenter = 1 - Math.abs(componentPercent.x);
+
+        gsap
+          .timeline({
+            defaults: { duration: 0.5, overwrite: "auto", ease: "power3.out" },
+          })
+          .to(
+            ".avatar",
+            {
+              rotation: gsap.utils.clamp(-2, 2, 5 * componentPercent.x),
+              duration: 0.5,
+            },
+            0
+          )
+          .to(
+            ".highlight",
+
+            {
+              opacity: distFromCenter - 0.7,
+              x: (-10 + 20) & componentPercent.x,
+              duration: 0.5,
+            },
+            0
+          );
+      };
+    }, component);
+  }, []);
 
   return (
     <div ref={component} className={clsx("relative h-full w-full", className)}>
