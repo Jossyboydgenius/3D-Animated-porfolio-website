cb592b9472406d874b1d8f2a604e32c6baaf1b01
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-05 18:42:24 +0000

    Commit

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -45,16 +45,17 @@ export default function ContentList({
             rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1),
             ease: "back.out(2)",
             duration: 1.3,
+            opacity: 1,
           });
         }
         lastMousePos.current = mousePos;
         return () => ctx.revert();
       }, component);
     };
-    window.addEventListener("mousemove", handleMouseMove)
+    window.addEventListener("mousemove", handleMouseMove);
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
-    }
+    };
   }, [currentItem]);
 
   const contentImages = items.map((item) => {
@@ -84,42 +85,44 @@ export default function ContentList({
         className="grid border-b border-b-slate-100"
         onMouseLeave={onMouseLeave}
       >
-        {items.map((item, index) => (
-          <React.Fragment key={index}>
-            {isFilled.keyText(item.data.title) && (
-              <li
-                key={index}
-                className="list-item opacity-0f"
-                onMouseEnter={() => onMouseEnter(index)}
-              >
-                <Link
-                  href={urlPrefix + "/" + item.uid}
-                  className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
-                  aria-label={item.data.title || ""}
+        {[...items, ...items, ...items, ...items, ...items, ...items].map(
+          (item, index) => (
+            <React.Fragment key={index}>
+              {isFilled.keyText(item.data.title) && (
+                <li
+                  key={index}
+                  className="list-item opacity-0f"
+                  onMouseEnter={() => onMouseEnter(index)}
                 >
-                  <div className="flex flex-col">
-                    <span className="text-3xl font-bold">
-                      {item.data.title}
-                    </span>
-                    <div className="flex gap-3 text-yellow-400 text-lg font-bold">
-                      {item.tags.map((tag, index) => (
-                        <span key={index}>{tag}</span>
-                      ))}
+                  <Link
+                    href={urlPrefix + "/" + item.uid}
+                    className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+                    aria-label={item.data.title || ""}
+                  >
+                    <div className="flex flex-col">
+                      <span className="text-3xl font-bold">
+                        {item.data.title}
+                      </span>
+                      <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                        {item.tags.map((tag, index) => (
+                          <span key={index}>{tag}</span>
+                        ))}
+                      </div>
                     </div>
-                  </div>
-                  <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
-                    {viewMoreText} <MdArrowOutward />
-                  </span>
-                </Link>
-              </li>
-            )}
-          </React.Fragment>
-        ))}
+                    <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
+                      {viewMoreText} <MdArrowOutward />
+                    </span>
+                  </Link>
+                </li>
+              )}
+            </React.Fragment>
+          )
+        )}
       </ul>
 
       {/* Hover Element */}
       <div
-        className="hover-reveal  pointer-events-none absolute left-0 top-0 -z-10 h-[320px] rounded-lg bg-over bg-center opacity-0f transition-[background] duration-300"
+        className="hover-reveal  pointer-events-none absolute left-0 top-0 -z-10 h-[320px] rounded-lg bg-over bg-center opacity-0 transition-[background] duration-300"
         style={{
           backgroundImage:
             currentItem !== null ? `url(${contentImages[currentItem]})` : "",
