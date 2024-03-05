85dd1876f2602dd02f0adc481f284fa60ed50f8c
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-05 22:59:13 +0000

    Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const urbanist = Urbanist({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={urbanist.className}>
         <Header />
         {children}
+        <Footer />
         <div className="h-[500vh]"></div>
       </body>
     </html>
