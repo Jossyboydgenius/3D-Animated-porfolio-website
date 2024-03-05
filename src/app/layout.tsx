ccf8d94f6f76d185f33a62b5c39ec13a3c638e67
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-05 23:40:03 +0000

    Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         <Header />
         {children}
         <Footer />
+        <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
         <div className="h-[500vh]"></div>
       </body>
     </html>
