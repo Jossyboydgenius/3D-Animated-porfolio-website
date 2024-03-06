939f8d181427056317b80b09826753e975007364
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 12:41:26 +0000

    Commit

--- src/slices/TextBlock/index.tsx
+++ src/slices/TextBlock/index.tsx
@@ -11,7 +11,7 @@ export type TextBlockProps = SliceComponentProps<Content.TextBlockSlice>;
  */
 const TextBlock = ({ slice }: TextBlockProps): JSX.Element => {
   return (
-    <div className="prose">
+    <div className="prose prose-invert">
    <PrismicRichText field={slice.primary.text} />
     </div>
   );
