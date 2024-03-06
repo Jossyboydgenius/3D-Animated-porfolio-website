c79729f4a2d8881d0bcff113aa85e5e7efec09ee
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 12:37:19 +0000

    Commit

--- src/slices/TextBlock/index.tsx
+++ src/slices/TextBlock/index.tsx
@@ -10,7 +10,9 @@ export type TextBlockProps = SliceComponentProps<Content.TextBlockSlice>;
  * Component for "TextBlock" Slices.
  */
 const TextBlock = ({ slice }: TextBlockProps): JSX.Element => {
-  return <PrismicRichText field={slice.primary.text} />;
+  return (
+   <PrismicRichText field={slice.primary.text} />
+  );
 };
 
 export default TextBlock;
