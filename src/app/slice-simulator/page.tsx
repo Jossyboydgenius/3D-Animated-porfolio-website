ebed603097745be9fc22b055948f16128e3df027
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 16:10:49 +0000

    Commit

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -8,6 +8,7 @@ import { components } from "../../slices";
 export default function SliceSimulatorPage() {
   return (
     <SliceSimulator
+      background="#121b2f"
       sliceZone={(props) => <SliceZone {...props} components={components} />}
     />
   );
