3f7d57fcfc1cd9d09acbeb6e2529eb0c9bfcb698
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 14:57:55 +0000

    Commit

--- src/slices/Experience/index.tsx
+++ src/slices/Experience/index.tsx
@@ -1,5 +1,7 @@
+import Bounded from "@/components/Bounded";
+import Heading from "@/components/Heading";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `Experience`.
@@ -11,12 +13,30 @@ export type ExperienceProps = SliceComponentProps<Content.ExperienceSlice>;
  */
 const Experience = ({ slice }: ExperienceProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for experience (variation: {slice.variation}) Slices
-    </section>
+      <Heading as="h2" size="lg">
+        {slice.primary.heading}
+      </Heading>
+      {slice.items.map((item, index) => (
+        <div key={index} className="ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
+          <Heading as="h3" size="sm">
+            {item.title}
+          </Heading>
+
+          <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400">
+            <span>{item.time_period}</span>{" "}
+            <span className="text-3xl font-extralight">/</span>{" "}
+            <span>{item.institution}</span>
+          </div>
+          <div className="prose prose-lg prose-invert mt-4">
+            <PrismicRichText field={item.description} />
+          </div>
+        </div>
+      ))}
+    </Bounded>
   );
 };
 
