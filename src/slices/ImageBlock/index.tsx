97abc2f876e580f439b24582b747c7b71cd97f77
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 14:07:01 +0000

    Commit

--- src/slices/ImageBlock/index.tsx
+++ src/slices/ImageBlock/index.tsx
@@ -1,4 +1,5 @@
 import { Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps } from "@prismicio/react";
 
 /**
@@ -11,13 +12,7 @@ export type ImageBlockProps = SliceComponentProps<Content.ImageBlockSlice>;
  */
 const ImageBlock = ({ slice }: ImageBlockProps): JSX.Element => {
   return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for image_block (variation: {slice.variation})
-      Slices
-    </section>
+    <PrismicNextImage field={slice.primary.image} imgixParams={{w: 600}}/>
   );
 };
 
