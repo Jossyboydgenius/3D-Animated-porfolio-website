cf95edfd6f5f0f906f287a8eb164e88f3f6edad6
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 12:23:46 +0000

    Commit

--- src/slices/TextBlock/index.tsx
+++ src/slices/TextBlock/index.tsx
@@ -1,5 +1,5 @@
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 
 /**
  * Props for `TextBlock`.
@@ -10,14 +10,7 @@ export type TextBlockProps = SliceComponentProps<Content.TextBlockSlice>;
  * Component for "TextBlock" Slices.
  */
 const TextBlock = ({ slice }: TextBlockProps): JSX.Element => {
-  return (
-    <section
-      data-slice-type={slice.slice_type}
-      data-slice-variation={slice.variation}
-    >
-      Placeholder component for text_block (variation: {slice.variation}) Slices
-    </section>
-  );
+  return <PrismicRichText field={slice.primary.text} />;
 };
 
 export default TextBlock;
