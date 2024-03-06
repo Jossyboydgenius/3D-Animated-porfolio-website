a53875df78eb4749de4811d655ae1b4ec18f4981
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 16:28:04 +0000

    Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
         <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div>
         <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
       </body>
+      <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
