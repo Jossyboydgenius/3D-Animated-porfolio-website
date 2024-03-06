ecf328eb0e076c925055e34c4bb4a3c3cbb0376a
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 13:16:33 +0000

    Commit

--- src/app/blog/[uid]/page.tsx
+++ src/app/blog/[uid]/page.tsx
@@ -6,6 +6,7 @@ import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
+import { DateField, isFilled } from "@prismicio/client";
 
 type Params = { uid: string };
 
@@ -14,22 +15,33 @@ export default async function Page({ params }: { params: Params }) {
   const page = await client
     .getByUID("blog_post", params.uid)
     .catch(() => notFound());
+    
+  function formatDate(date: DateField){
+    if (isFilled.date(date)){
+
+        const dateOptions: Intl.DateTimeFormatOptions = {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+    day: "numeric",
+};
+    return new Intl.DateTimeFormat("en-US", dateOptions).format(new Date(date))
+}  
+}
 
   return (
-  <Bounded as="article" >
-    <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
+    <Bounded as="article">
+      <div className="rounded-2xl border-2 border-slate-800 bg-slate-900 px-4 py-10 md:px-8 md:py-20">
         <Heading as="h1">{page.data.title}</Heading>
         <div className="flex gap-4 text-yellow-400 text-xl font-bold">
-            {page.tags.map((tag)=>(
-                <span key={tag}>{tag}</span>
-            ))}
+          {page.tags.map((tag) => (
+            <span key={tag}>{tag}</span>
+          ))}
         </div>
-        <p>
-            {page.data.date}
-        </p>
-  <SliceZone slices={page.data.slices} components={components} />
-    </div>
-  </Bounded>
+        <p>{page.data.date}</p>
+        <SliceZone slices={page.data.slices} components={components} />
+      </div>
+    </Bounded>
   );
 }
 
