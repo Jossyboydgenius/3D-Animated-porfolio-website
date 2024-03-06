b75d51bea16c26fbcb6273e510fc47b9fa855488
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-03-06 18:03:45 +0000

    Commit

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,19 +1,20 @@
 "use client";
 
-import React, { useRef, useState, useEffect } from "react";
-import { asImageSrc, isFilled } from "@prismicio/client";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Content, asImageSrc, isFilled } from "@prismicio/client";
+import Link from "next/link";
+import React, { useEffect, useRef, useState } from "react";
 import { MdArrowOutward } from "react-icons/md";
-import { Content } from "@prismicio/client";
+import { gsap } from "gsap/gsap-core";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { url } from "inspector";
 
 gsap.registerPlugin(ScrollTrigger);
 
 type ContentListProps = {
   items: Content.BlogPostDocument[] | Content.ProjectDocument[];
-  contentType: Content.BlogPostIndexSlice["primary"]["content_type"];
-  fallbackItemImage: Content.BlogPostIndexSlice["primary"]["fallback_item_image"];
-  viewMoreText: Content.BlogPostIndexSlice["primary"]["view_more_text"];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
 };
 
 export default function ContentList({
@@ -23,54 +24,46 @@ export default function ContentList({
   viewMoreText = "Read More",
 }: ContentListProps) {
   const component = useRef(null);
-  const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
-
   const revealRef = useRef(null);
+  const itemsRef = useRef<Array<HTMLLIElement | null>>([]);
   const [currentItem, setCurrentItem] = useState<null | number>(null);
-  const [hovering, setHovering] = useState(false);
+
   const lastMousePos = useRef({ x: 0, y: 0 });
 
-  const urlPrefix = contentType === "Blogs" ? "/blog" : "/project";
+  const urlPrefix = contentType === "Blog" ? "/blog" : "/projects";
 
   useEffect(() => {
-    // Animate list-items in with a stagger
     let ctx = gsap.context(() => {
-      itemsRef.current.forEach((item, index) => {
+      itemsRef.current.forEach((item) => {
         gsap.fromTo(
           item,
-          {
-            opacity: 0,
-            y: 20,
-          },
+          { opacity: 0, y: 20 },
           {
             opacity: 1,
             y: 0,
             duration: 1.3,
             ease: "elastic.out(1,0.3)",
-            stagger: 0.2,
             scrollTrigger: {
               trigger: item,
-              start: "top bottom-=100px",
-              end: "bottom center",
+              start: "top botton-=100px",
+              end: "botton center",
               toggleActions: "play none none none",
             },
-          },
+          }
         );
       });
-
-      return () => ctx.revert(); // cleanup!
+      return () => ctx.revert();
     }, component);
   }, []);
 
   useEffect(() => {
-    // Mouse move event listener
     const handleMouseMove = (e: MouseEvent) => {
       const mousePos = { x: e.clientX, y: e.clientY + window.scrollY };
-      // Calculate speed and direction
+
+      //Calculate speed and direction
       const speed = Math.sqrt(Math.pow(mousePos.x - lastMousePos.current.x, 2));
 
       let ctx = gsap.context(() => {
-        // Animate the image holder
         if (currentItem !== null) {
           const maxY = window.scrollY + window.innerHeight - 350;
           const maxX = window.innerWidth - 250;
@@ -78,43 +71,28 @@ export default function ContentList({
           gsap.to(revealRef.current, {
             x: gsap.utils.clamp(0, maxX, mousePos.x - 110),
             y: gsap.utils.clamp(0, maxY, mousePos.y - 160),
-            rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1), // Apply rotation based on speed and direction
+            rotation: speed * (mousePos.x > lastMousePos.current.x ? 1 : -1),
             ease: "back.out(2)",
             duration: 1.3,
-          });
-          gsap.to(revealRef.current, {
-            opacity: hovering ? 1 : 0,
-            visibility: "visible",
-            ease: "power3.out",
-            duration: 0.4,
+            opacity: 1,
           });
         }
         lastMousePos.current = mousePos;
-        return () => ctx.revert(); // cleanup!
+        return () => ctx.revert();
       }, component);
     };
-
     window.addEventListener("mousemove", handleMouseMove);
-
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, [hovering, currentItem]);
+  }, [currentItem]);
 
-  const onMouseEnter = (index: number) => {
-    setCurrentItem(index);
-    if (!hovering) setHovering(true);
-  };
-
-  const onMouseLeave = () => {
-    setHovering(false);
-    setCurrentItem(null);
-  };
 
   const contentImages = items.map((item) => {
-    const image = isFilled.image(item.data.image)
-      ? item.data.image
+    const image = isFilled.image(item.data.hover_image)
+      ? item.data.hover_image
       : fallbackItemImage;
+
     return asImageSrc(image, {
       fit: "crop",
       w: 220,
@@ -123,61 +101,71 @@ export default function ContentList({
     });
   });
 
-  // Preload images
-  useEffect(() => {
-    contentImages.forEach((url) => {
+  useEffect(()=>{
+    contentImages.forEach((url)=>{
       if (!url) return;
       const img = new Image();
       img.src = url;
-    });
-  }, [contentImages]);
+    })
+  }, [contentImages])
+
+  const onMouseEnter = (index: number) => {
+    setCurrentItem(index);
+  };
+
+  const onMouseLeave = () => {
+    setCurrentItem(null);
+  };
 
   return (
-    <>
+    <div ref={component}>
       <ul
-        ref={component}
         className="grid border-b border-b-slate-100"
         onMouseLeave={onMouseLeave}
       >
-        {items.map((post, index) => (
-          <li
-            key={index}
-            ref={(el) => (itemsRef.current[index] = el)}
-            onMouseEnter={() => onMouseEnter(index)}
-            className="list-item opacity-0"
-          >
-            <a
-              href={`${urlPrefix}/${post.uid}`}
-              className="flex flex-col justify-between border-t border-t-slate-100 py-10  text-slate-200 md:flex-row "
-              aria-label={post.data.title || ""}
-            >
-              <div className="flex flex-col">
-                <span className="text-3xl font-bold">{post.data.title}</span>
-                <div className="flex gap-3 text-yellow-400">
-                  {post.tags.map((tag, index) => (
-                    <span key={index} className="text-lg font-bold">
-                      {tag}
+        {items.map((item, index) => (
+          <>
+            {isFilled.keyText(item.data.title) && (
+              <li
+                key={index}
+                className="list-item opacity-0"
+                onMouseEnter={() => onMouseEnter(index)}
+                ref={(el) => (itemsRef.current[index] = el)}
+              >
+                <Link
+                  href={urlPrefix + "/" + item.uid}
+                  className="flex flex-col justify-between border-t border-t-slate-100 py-10 text-slate-200 md:flex-row"
+                  aria-label={item.data.title || ""}
+                >
+                  <div className="flex flex-col">
+                    <span className="text-3xl font-bold">
+                      {item.data.title}
                     </span>
-                  ))}
-                </div>
-              </div>
-              <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
-                {viewMoreText} <MdArrowOutward />
-              </span>
-            </a>
-          </li>
+                    <div className="flex gap-3 text-yellow-400 text-lg font-bold">
+                      {item.tags.map((tag, index) => (
+                        <span key={index}>{tag}</span>
+                      ))}
+                    </div>
+                  </div>
+                  <span className="ml-auto flex items-center gap-2 text-xl font-medium md:ml-0">
+                    {viewMoreText} <MdArrowOutward />
+                  </span>
+                </Link>
+              </li>
+            )}
+          </>
         ))}
-
-        {/* Hover element */}
-        <div
-          className="hover-reveal pointer-events-none absolute left-0 top-0 -z-10 h-[320px] w-[220px] rounded-lg bg-cover bg-center opacity-0 transition-[background] duration-300"
-          style={{
-            backgroundImage:
-              currentItem !== null ? `url(${contentImages[currentItem]})` : "",
-          }}
-          ref={revealRef}
-        ></div>
       </ul>
-    </>
+
+      {/* Hover Element */}
+      <div
+        className="hover-reveal  pointer-events-none absolute left-0 top-0 -z-10 h-[320px] rounded-lg bg-over bg-center opacity-0 transition-[background] duration-300"
+        style={{
+          backgroundImage:
+            currentItem !== null ? `url(${contentImages[currentItem]})` : "",
+        }}
+        ref={revealRef}
+      ></div>
+    </div>
   );
 }
--- src/slices/TechList/index.tsx
+++ src/slices/TechList/index.tsx
@@ -2,10 +2,11 @@
 
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
-import React, { useEffect, useRef } from "react";
+import React, { useLayoutEffect, useRef } from "react";
 import { MdCircle } from "react-icons/md";
-import { gsap } from "gsap/gsap-core";
+import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import Bounded from "@/components/Bounded";
 import Heading from "@/components/Heading";
 
@@ -22,13 +23,14 @@ export type TechListProps = SliceComponentProps<Content.TechListSlice>;
 const TechList = ({ slice }: TechListProps): JSX.Element => {
   const component = useRef(null);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     let ctx = gsap.context(() => {
+      // create as many GSAP animations and/or ScrollTriggers here as you want...
       const tl = gsap.timeline({
         scrollTrigger: {
-          trigger: component.current,
+          pin: true, // pin the trigger element while active
           start: "top bottom",
-          end: "bottom top,",
+          end: "bottom top",
           scrub: 4,
         },
       });
@@ -49,34 +51,37 @@ const TechList = ({ slice }: TechListProps): JSX.Element => {
               : gsap.utils.random(600, 400);
           },
           ease: "power1.inOut",
-        }
+        },
       );
     }, component);
-    return () => ctx.revert(); //cleanup!
-  });
+    return () => ctx.revert(); // cleanup!
+  }, []);
 
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="overflow-hidden"
+      className="wrapper overflow-hidden"
       ref={component}
     >
       <Bounded as="div">
-        <Heading size="lg" className="mb-8" as="h2">
-          {slice.primary.heading}
+        <Heading size="xl" className="mb-8" as="h2">
+          {slice.primary.title}
         </Heading>
       </Bounded>
+
       {slice.items.map(({ tech_color, tech_name }, index) => (
         <div
           key={index}
           className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
-          aria-label={tech_name || undefined}
+          aria-label={tech_name || ""}
         >
           {Array.from({ length: 15 }, (_, index) => (
             <React.Fragment key={index}>
               <span
-                className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                className={
+                  "tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                }
                 style={{
                   color: index === 7 && tech_color ? tech_color : "inherit",
                 }}
