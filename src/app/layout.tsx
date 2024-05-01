ff9c284681f7cecc44aeb5c359fba9ae40e506ff
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-05-01 14:37:44 +0000

    Update title and description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export async function generateMetadata(): Promise<Metadata> {
   const settings = await client.getSingle("settings");
 
   return {
-    title: settings.data.meta_title,
-    description: settings.data.meta_description,
+    title: "Awe Joseph",
+    description: "Portfolio website",
   };
 }
 
